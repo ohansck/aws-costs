ffcc94889d9d68f99fb8a63a234019ea2f27216e
ohansck <ohansck@gmail.com>
2025-12-25 01:11:51 +0100

    Initial commit

---
+++ test/aws-costs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCosts from '../lib/aws-costs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-costs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCosts.AwsCostsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
